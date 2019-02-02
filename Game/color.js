37792e98576a97808adb4d5387d409b3dbb2a29d
Shubham Anand <shubham_chess@live.com>
2019-02-03 01:24:52 +0530

    hex codes for colors

---
+++ Game/color.js
@@ -0,0 +1,11 @@
+// Contains color codes of various colors used in the game
+
+let BROWN = 'D2691E';
+let LIGHT_BLUE = '8470FF';
+let PINK = 'FFC0CB';
+let ORANGE = 'FF4500';
+let RED = 'FF0000';
+let YELLOW = 'FFFF00';
+let GREEN = '7CFC00';
+let BLUE = '4169E1';
+let WHITE = 'FFFFFF';
