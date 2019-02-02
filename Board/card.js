3cb17b7a3e5bf4000c2b2357b60201e87c151741
Shubham Anand <shubham_chess@live.com>
2019-02-02 23:41:02 +0530

    init card object

---
+++ Board/card.js
@@ -0,0 +1,8 @@
+class Card {
+    constructor(id, title, description, action) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.action = action;
+    }
+};
