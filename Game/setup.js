9088486f7c859b067a23fa97b4b3fc73520ab74a
Shubham Anand <shubham_chess@live.com>
2019-02-03 21:15:05 +0530

    do board and card setup

--- Game/setup.js
+++ Game/setup.js
@@ -1,10 +1,24 @@
-function boardSetup(region="UK") {
+function boardSetup(gameMode="UK") {
     let board = [];
+    let boardMap = gameModes[gameMode]["locations"];
+    let action = function() {
+
+    };
+    for (let key in boardMap) {
+        let square = new Square(key, boardMap[key]["title"], boardMap[key]["value"], boardMap[key]["housePrice"], boardMap[key]["picture"], boardMap[key]["color"], rentScheme[key], action)
+        board.push(square);
+    }
     return board;
 }
 
-function cardSetup(type, region="UK") {
-    return CARDS[type](region);
+function cardSetup(type, gameMode="UK") {
+    let cards = [];
+    let cardDict = CARDS[type](gameMode);
+    for (let key in cardDict) {
+        let card = new Card(key, type, cardDict[key]["desc"], cardDict[key]["action"]);
+        cards.push(card);
+    }
+    return cards;
 }
 
 function diceSetup() {
