095356852fa85080dbffdbfd4cf4f48813d0d20e
Shubham Anand <shubham_chess@live.com>
2019-02-09 16:35:52 +0530

    complete board setup

--- Game/setup.js
+++ Game/setup.js
@@ -1,13 +1,62 @@
 function boardSetup(gameMode="UK") {
     let board = [];
     let boardMap = gameModes[gameMode]["locations"];
-    let action = function() {
 
-    };
+    // type 1 sqaures
+
     for (let key in boardMap) {
         let square = new Square(1, key, boardMap[key]["title"], boardMap[key]["picture"], boardMap[key]["color"], boardMap[key]["value"], boardMap[key]["housePrice"], rentScheme[key])
-        board.push(square);
+        board[key] = square;
     }
+
+    // type 0 : corner squares
+
+    board[0] = new Square(0, 0, "GO", "assets/common/go.jpg", action = function(args){
+        // collect money when someone passes go
+    });
+    board[10] = new Square(0, 9, "JAIL", "assets/common/jail.jpg", action = function(args){
+        // Do nothing if player comes to jail directly
+    });
+    board[20] = new Square(0, 14, "FREE PARKING", "assets/common/parking.jpg", action = function(args){
+        // Player recieves unit 20 money
+    });
+    board[30] = new Square(0, 19, "GO TO JAIL", "assets/common/gojail.jpg", action = function(args){
+        // Player doesnt pass go and goes directly to Jail.
+    });
+
+    // type 0 : tax squares
+
+    board[4] = new Square(0, 3, "Income Tax", "assets/common/incometax.jpg", action = function(args) {
+        // Player gets a deduction of 200 units of money
+    });
+    board[38] = new Square(0, 38, "Luxury Tax", "assets/common/luxurytax.jpg", action = function(args) {
+        // Player gets a deduction of 100 units of money
+    });
+
+    // type 0 : community chest
+
+    board[2] = new Square(0, 2, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+        // open a community chest
+    });
+    board[17] = new Square(0, 17, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+        // open a community chest
+    });
+    board[33] = new Square(0, 33, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+        // open a community chest
+    });
+
+    // type 0 : chance
+
+    board[7] = new Square(0, 7, "Chance", "assets/common/chance.jpg", action = function(args) {
+        // open a Chance
+    });
+    board[22] = new Square(0, 22, "Chance", "assets/common/chance.jpg", action = function(args) {
+        // open a Chance
+    });
+    board[36] = new Square(0, 36, "Chance", "assets/common/chance.jpg", action = function(args) {
+        // open a Chance
+    });
+
     return board;
 }
 
