6e635d7d1433af77ecc8d6657285d94712ffd7d4
Shubham Anand <shubham_chess@live.com>
2019-02-09 16:35:40 +0530

    comment out players

--- Game/game.js
+++ Game/game.js
@@ -4,7 +4,7 @@ class Game {
         this.communityChest = cardSetup("COMMUNITY_CHEST");
         this.chance = cardSetup("CHANCE");
         this.dice = diceSetup();
-        this.players = playerSetup(playersArr, this.dice);
+        // this.players = playerSetup(playersArr, this.dice);
         // the playing order of players is same as indexing of the players array
         this.currentPlayer = 0;
         // Initially player[0] will make his move
