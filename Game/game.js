967c49f44e88515d9b292238cfc8e17e4bd1f16d
Shubham Anand <shubham_chess@live.com>
2019-02-11 12:24:42 +0530

    add player to setup

--- Game/game.js
+++ Game/game.js
@@ -1,18 +1,14 @@
 class Game {
-    constructor(playersArr, playingOrder) {
+    constructor(playersArr) {
         this.board = boardSetup();
         this.communityChest = cardSetup("COMMUNITY_CHEST");
         this.chance = cardSetup("CHANCE");
-        this.dice = diceSetup();
-        // this.players = playerSetup(playersArr, this.dice);
+        this.dices = dicesSetup();
         // the playing order of players is same as indexing of the players array
-        this.currentPlayer = 0;
+        this.players = playerSetup(playersArr);
         // Initially player[0] will make his move
+        this.currentPlayer = 0;
         this.moves = 0;
-    }
-
-    roll() {
-        return [this.dice[0].roll(), this.dice[1].roll()];
     }
 
 };
