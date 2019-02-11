00333daa32d8af99f20f230c96061b570da3d860
Shubham Anand <shubham_chess@live.com>
2019-02-11 12:24:12 +0530

    replace dice with dices object

--- Game/setup.js
+++ Game/setup.js
@@ -11,49 +11,49 @@ function boardSetup(gameMode="UK") {
 
     // type 0 : corner squares
 
-    board[0] = new Square(0, 0, "GO", "assets/common/go.jpg", action = function(args){
+    board[0] = new Square(0, 0, "GO", "assets/common/go.jpg", undefined, undefined, undefined, undefined, function(args){
         // collect money when someone passes go
     });
-    board[10] = new Square(0, 9, "JAIL", "assets/common/jail.jpg", action = function(args){
+    board[10] = new Square(0, 9, "JAIL", "assets/common/jail.jpg", undefined, undefined, undefined, undefined, function(args){
         // Do nothing if player comes to jail directly
     });
-    board[20] = new Square(0, 14, "FREE PARKING", "assets/common/parking.jpg", action = function(args){
+    board[20] = new Square(0, 14, "FREE PARKING", "assets/common/parking.jpg", undefined, undefined, undefined, undefined, function(args){
         // Player recieves unit 20 money
     });
-    board[30] = new Square(0, 19, "GO TO JAIL", "assets/common/gojail.jpg", action = function(args){
+    board[30] = new Square(0, 19, "GO TO JAIL", "assets/common/gojail.jpg", undefined, undefined, undefined, undefined, function(args){
         // Player doesnt pass go and goes directly to Jail.
     });
 
     // type 0 : tax squares
 
-    board[4] = new Square(0, 3, "Income Tax", "assets/common/incometax.jpg", action = function(args) {
+    board[4] = new Square(0, 3, "Income Tax", "assets/common/incometax.jpg", undefined, undefined, undefined, undefined, function(args) {
         // Player gets a deduction of 200 units of money
     });
-    board[38] = new Square(0, 38, "Luxury Tax", "assets/common/luxurytax.jpg", action = function(args) {
+    board[38] = new Square(0, 38, "Luxury Tax", "assets/common/luxurytax.jpg", undefined, undefined, undefined, undefined, function(args) {
         // Player gets a deduction of 100 units of money
     });
 
     // type 0 : community chest
 
-    board[2] = new Square(0, 2, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+    board[2] = new Square(0, 2, "Community Chest", "assets/common/communitychest.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a community chest
     });
-    board[17] = new Square(0, 17, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+    board[17] = new Square(0, 17, "Community Chest", "assets/common/communitychest.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a community chest
     });
-    board[33] = new Square(0, 33, "Community Chest", "assets/common/communitychest.jpg", action = function(args) {
+    board[33] = new Square(0, 33, "Community Chest", "assets/common/communitychest.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a community chest
     });
 
     // type 0 : chance
 
-    board[7] = new Square(0, 7, "Chance", "assets/common/chance.jpg", action = function(args) {
+    board[7] = new Square(0, 7, "Chance", "assets/common/chance.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a Chance
     });
-    board[22] = new Square(0, 22, "Chance", "assets/common/chance.jpg", action = function(args) {
+    board[22] = new Square(0, 22, "Chance", "assets/common/chance.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a Chance
     });
-    board[36] = new Square(0, 36, "Chance", "assets/common/chance.jpg", action = function(args) {
+    board[36] = new Square(0, 36, "Chance", "assets/common/chance.jpg", undefined, undefined, undefined, undefined, function(args) {
         // open a Chance
     });
 
@@ -70,18 +70,22 @@ function cardSetup(type, gameMode="UK") {
     return cards;
 }
 
-function diceSetup() {
-    let dice = [];
-    for (let i=0; i<2; i++) {
-        dice[i] = new Dice();
-    }
-    return dice;
+function dicesSetup() {
+    let randomFunction1 = function() {
+        return Math.floor(Math.random()*6) + 1;
+    };
+    let randomFunction2 = function() {
+        return Math.floor(Math.random()*6) + 1;
+    };
+    return new Dices(randomFunction1, randomFunction2);
 }
 
-function playerSetup(playersArr, dice) {
+function playerSetup(playersArr) {
     let players = [];
-    for (let i=0; i<playersArr.lenth; i++) {
-        players[i] = new Player(playersArr[i].tokenid, i+1, playersArr[i].name, dice);
+    console.log(playersArr);
+    for (let i=0; i<playersArr.length; i++) {
+        console.log("hey");
+        players[i] = new Player(playersArr[i].tokenid, i+1, playersArr[i].name);
     }
     return players;
 }
