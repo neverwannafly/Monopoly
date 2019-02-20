de5098a2bd158a277f342c7d44ef344ed09bb74e
Shubham Anand <shubham_chess@live.com>
2019-02-20 21:01:55 +0530

    add rentScheme attribute and USA game mode

--- Game/config.js
+++ Game/config.js
@@ -9,6 +9,7 @@ let gameModes = {
                 housePrice: 30,
                 color: BROWN,
                 picture: "assets/uk/old_kent_road.jpg",
+                rentScheme: [0,2,10,30,90,160,250], 
             },
             3: {
                 title: "Whitechapel Road",
@@ -16,6 +17,7 @@ let gameModes = {
                 housePrice: 30,
                 color: BROWN,
                 picture: "assets/uk/whitechapel_road.jpg",
+                rentScheme: [0,4,20,60,180,320,450],
             },
             5: {
                 title: "King's Cross station",
@@ -23,6 +25,7 @@ let gameModes = {
                 housePrice: null,
                 color: WHITE,
                 picture: "assets/uk/king_cross_station.jpg",
+                rentScheme: [25,50,100,200],
             },
             6: {
                 title: "The Angel, Islington",
@@ -30,6 +33,7 @@ let gameModes = {
                 housePrice: 50,
                 color: LIGHT_BLUE,
                 picture: "assets/uk/islington.jpg",
+                rentScheme: [0,6,30,90,270,400,550],
             },
             8: {
                 title: "Euston Road",
@@ -37,6 +41,7 @@ let gameModes = {
                 housePrice: 50,
                 color: LIGHT_BLUE,
                 picture: "assets/uk/euston_road.jpg",
+                rentScheme: [0,6,30,90,270,400,550],
             },
             9: {
                 title: "Pentonville Road",
@@ -44,6 +49,7 @@ let gameModes = {
                 housePrice: 60,
                 color: LIGHT_BLUE,
                 picture: "assets/uk/pentonville_road.jpg",
+                rentScheme: [0,8,40,100,300,450,600],
             },
             11: {
                 title: "Pall Mall",
@@ -51,13 +57,15 @@ let gameModes = {
                 housePrice: 70,
                 color: PINK,
                 picture: "assets/uk/pall_mall.jpg",
+                rentScheme: [0,10,50,150,450,625,750],
             },
             12: {
                 title: "Electric Company",
                 value: 150,
                 housePrice: null,
-                color: WHITE,
+                color: SILVER,
                 picture: "assets/uk/electric_company.jpg",
+                rentScheme: [4,10],
             },
             13: {
                 title: "Whitehall",
@@ -65,6 +73,7 @@ let gameModes = {
                 housePrice: 70,
                 color: PINK,
                 picture: "assets/uk/whitehall.jpg",
+                rentScheme: [0,10,50,150,450,625,750],
             },
             14: {
                 title: "Northumberland Avenue",
@@ -72,6 +81,7 @@ let gameModes = {
                 housePrice: 80,
                 color: PINK,
                 picture: "assets/uk/northumberland_avenue.jpg",
+                rentScheme: [0,12,60,180,500,700,900],
             },
             15: {
                 title: "Marylebone Station",
@@ -79,6 +89,7 @@ let gameModes = {
                 housePrice: null,
                 color: WHITE,
                 picture: "assets/uk/marylebone_station.jpg",
+                rentScheme: [25,50,100,200],
             },
             16: {
                 title: "Bow Street",
@@ -86,6 +97,7 @@ let gameModes = {
                 housePrice: 90,
                 color: ORANGE,
                 picture: "assets/uk/bow_street.jpg",
+                rentScheme: [0,14,70,200,550,750,950],
             },
             18: {
                 title: "Great Marlborough Street",
@@ -93,6 +105,7 @@ let gameModes = {
                 housePrice: 100,
                 color: ORANGE,
                 picture: "assets/uk/marlborough_street.jpg",
+                rentScheme: [0,14,70,200,550,750,950],
             },
             19: {
                 title: "Vine Street",
@@ -100,6 +113,7 @@ let gameModes = {
                 housePrice: 100,
                 color: ORANGE,
                 picture: "assets/uk/vine_street.jpg",
+                rentScheme: [0,16,80,220,600,800,1000],
             },
             21: {
                 title: "Strand",
@@ -107,6 +121,7 @@ let gameModes = {
                 housePrice: 110,
                 color: RED,
                 picture: "assets/uk/strand.jpg",
+                rentScheme: [0,18,90,250,700,875,1050]
             },
             23: {
                 title: "Fleet Street",
@@ -114,6 +129,7 @@ let gameModes = {
                 housePrice: 110,
                 color: RED,
                 picture: "assets/uk/fleet_street.jpg",
+                rentScheme: [0,18,90,250,700,875,1050],
             },
             24: {
                 title: "Trafalgar Square",
@@ -121,6 +137,7 @@ let gameModes = {
                 housePrice: 120,
                 color: RED,
                 picture: "assets/uk/trafalgar_square.jpg",
+                rentScheme: [0,20,100,300,750,925,1100],
             },
             25: {
                 title: "Fenchurch Street station",
@@ -128,6 +145,7 @@ let gameModes = {
                 housePrice: null,
                 color: WHITE,
                 picture: "assets/uk/fenchurch_street_station.jpg",
+                rentScheme: [25,50,100,200],
             },
             26: {
                 title: "Leicester Square",
@@ -135,6 +153,7 @@ let gameModes = {
                 housePrice: 130,
                 color: YELLOW,
                 picture: "assets/uk/leicester_square.jpg",
+                rentScheme: [0,22,110,330,800,975,1150],
             },
             27: {
                 title: "Coventry Street",
@@ -142,13 +161,15 @@ let gameModes = {
                 housePrice: 130,
                 color: YELLOW,
                 picture: "assets/uk/coventry_street.jpg",
+                rentScheme: [0,22,110,330,800,975,1150],
             },
             28: {
                 title: "Electric Works",
                 value: 150,
                 housePrice: null,
-                color: WHITE,
+                color: SILVER,
                 picture: "assets/uk/electric_works.jpg",
+                rentScheme: [4,10],
             },
             29: {
                 title: "Piccadilly",
@@ -156,6 +177,7 @@ let gameModes = {
                 housePrice: 140,
                 color: YELLOW,
                 picture: "assets/uk/piccadilly.jpg",
+                rentScheme: [0,24,120,360,850,1025,1200],
             },
             31: {
                 title: "Regent Street",
@@ -163,6 +185,7 @@ let gameModes = {
                 housePrice: 150,
                 color: GREEN,
                 picture: "assets/uk/regent_street.jpg",
+                rentScheme: [0,26,130,390,900,1100,1275],
             },
             32: {
                 title: "Oxford Street",
@@ -170,6 +193,7 @@ let gameModes = {
                 housePrice: 150,
                 color: GREEN,
                 picture: "assets/uk/oxford_street.jpg",
+                rentScheme: [0,26,130,390,900,1100,1275],
             },
             34: {
                 title: "Bond Street",
@@ -177,6 +201,7 @@ let gameModes = {
                 housePrice: 160,
                 color: GREEN,
                 picture: "assets/uk/bond_street.jpg",
+                rentScheme: [0,28,150,450,1000,1200,1400],
             },
             35: {
                 title: "Liverpool Street Station",
@@ -184,6 +209,7 @@ let gameModes = {
                 housePrice: null,
                 color: WHITE,
                 picture: "assets/uk/liverpool_street_station.jpg",
+                rentScheme: [25,50,100,200],
             },
             37: {
                 title: "Park Lane",
@@ -191,6 +217,7 @@ let gameModes = {
                 housePrice: 175,
                 color: BLUE,
                 picture: "assets/uk/park_lane.jpg",
+                rentScheme: [0,35,175,500,1100,1300,1500],
             },
             39: {
                 title: "Mayfair",
@@ -198,18 +225,242 @@ let gameModes = {
                 housePrice: 200,
                 color: BLUE,
                 picture: "assets/uk/mayfair.jpg",
+                rentScheme: [0,50,200,600,1400,1700,2000],
             },
         },
         currency: "£",
         multiplier: 1,
     },
     USA: {
-
+        locations: {
+            1: {
+                title: "Mediterranean Avenue",
+                value: 60,
+                housePrice: 30,
+                color: BROWN,
+                picture: "assets/usa/mediterranean_avenue.jpg",
+                rentScheme: [0,2,10,30,90,160,250], 
+            },
+            3: {
+                title: "Baltic Avenue",
+                value: 60,
+                housePrice: 30,
+                color: BROWN,
+                picture: "assets/usa/baltic_avenue.jpg",
+                rentScheme: [0,4,20,60,180,320,450],
+            },
+            5: {
+                title: "King's Cross station",
+                value: 200,
+                housePrice: null,
+                color: WHITE,
+                picture: "assets/usa/king_cross_station.jpg",
+                rentScheme: [25,50,100,200],
+            },
+            6: {
+                title: "Oriental Avenue",
+                value: 100,
+                housePrice: 50,
+                color: LIGHT_BLUE,
+                picture: "assets/usa/oriental_avenue.jpg",
+                rentScheme: [0,6,30,90,270,400,550],
+            },
+            8: {
+                title: "Vermont Avenue",
+                value: 100,
+                housePrice: 50,
+                color: LIGHT_BLUE,
+                picture: "assets/usa/vermont_avenue.jpg",
+                rentScheme: [0,6,30,90,270,400,550],
+            },
+            9: {
+                title: "Connecticut Avenue",
+                value: 120,
+                housePrice: 60,
+                color: LIGHT_BLUE,
+                picture: "assets/usa/connecticut_avenue.jpg",
+                rentScheme: [0,8,40,100,300,450,600],
+            },
+            11: {
+                title: "St. Charles Place",
+                value: 140,
+                housePrice: 70,
+                color: PINK,
+                picture: "assets/usa/st_charles_place.jpg",
+                rentScheme: [0,10,50,150,450,625,750],
+            },
+            12: {
+                title: "Electric Company",
+                value: 150,
+                housePrice: null,
+                color: SILVER,
+                picture: "assets/usa/electric_company.jpg",
+                rentScheme: [4,10],
+            },
+            13: {
+                title: "States Avenue",
+                value: 140,
+                housePrice: 70,
+                color: PINK,
+                picture: "assets/usa/states_avenue.jpg",
+                rentScheme: [0,10,50,150,450,625,750],
+            },
+            14: {
+                title: "Virginia Avenue",
+                value: 160,
+                housePrice: 80,
+                color: PINK,
+                picture: "assets/usa/virginia_avenue.jpg",
+                rentScheme: [0,12,60,180,500,700,900],
+            },
+            15: {
+                title: "Marylebone Station",
+                value: 200,
+                housePrice: null,
+                color: WHITE,
+                picture: "assets/usa/marylebone_station.jpg",
+                rentScheme: [25,50,100,200],
+            },
+            16: {
+                title: "St. James Place",
+                value: 180,
+                housePrice: 90,
+                color: ORANGE,
+                picture: "assets/usa/st_james_place.jpg",
+                rentScheme: [0,14,70,200,550,750,950],
+            },
+            18: {
+                title: "Tennessee Avenue",
+                value: 180,
+                housePrice: 100,
+                color: ORANGE,
+                picture: "assets/usa/tennessee_avenue.jpg",
+                rentScheme: [0,14,70,200,550,750,950],
+            },
+            19: {
+                title: "New York Avenue",
+                value: 200,
+                housePrice: 100,
+                color: ORANGE,
+                picture: "assets/usa/new_york_avenue.jpg",
+                rentScheme: [0,16,80,220,600,800,1000],
+            },
+            21: {
+                title: "Kentucky Avenue",
+                value: 220,
+                housePrice: 110,
+                color: RED,
+                picture: "assets/usa/kentucky_avenue.jpg",
+                rentScheme: [0,18,90,250,700,875,1050]
+            },
+            23: {
+                title: "Indiana Avenue",
+                value: 220,
+                housePrice: 110,
+                color: RED,
+                picture: "assets/usa/indiana_avenue.jpg",
+                rentScheme: [0,18,90,250,700,875,1050],
+            },
+            24: {
+                title: "Illinois Avenue",
+                value: 240,
+                housePrice: 120,
+                color: RED,
+                picture: "assets/usa/illinois_avenue.jpg",
+                rentScheme: [0,20,100,300,750,925,1100],
+            },
+            25: {
+                title: "Fenchurch Street station",
+                value: 200,
+                housePrice: null,
+                color: WHITE,
+                picture: "assets/usa/fenchurch_street_station.jpg",
+                rentScheme: [25,50,100,200],
+            },
+            26: {
+                title: "Atlantic Avenue",
+                value: 260,
+                housePrice: 130,
+                color: YELLOW,
+                picture: "assets/usa/atlantic_avenue.jpg",
+                rentScheme: [0,22,110,330,800,975,1150],
+            },
+            27: {
+                title: "Ventnor Avenue",
+                value: 260,
+                housePrice: 130,
+                color: YELLOW,
+                picture: "assets/usa/ventnor_avenue.jpg",
+                rentScheme: [0,22,110,330,800,975,1150],
+            },
+            28: {
+                title: "Electric Works",
+                value: 150,
+                housePrice: null,
+                color: SILVER,
+                picture: "assets/usa/electric_works.jpg",
+                rentScheme: [4,10],
+            },
+            29: {
+                title: "Marvin Gardens",
+                value: 280,
+                housePrice: 140,
+                color: YELLOW,
+                picture: "assets/usa/marvin_gardens.jpg",
+                rentScheme: [0,24,120,360,850,1025,1200],
+            },
+            31: {
+                title: "Pacific Avenue",
+                value: 300,
+                housePrice: 150,
+                color: GREEN,
+                picture: "assets/usa/pacific_avenue.jpg",
+                rentScheme: [0,26,130,390,900,1100,1275],
+            },
+            32: {
+                title: "North Carolina Avenue",
+                value: 300,
+                housePrice: 150,
+                color: GREEN,
+                picture: "assets/usa/north_carolina_avenue.jpg",
+                rentScheme: [0,26,130,390,900,1100,1275],
+            },
+            34: {
+                title: "Pennsylvania Avenue",
+                value: 320,
+                housePrice: 160,
+                color: GREEN,
+                picture: "assets/usa/pennsylvania_avenue.jpg",
+                rentScheme: [0,28,150,450,1000,1200,1400],
+            },
+            35: {
+                title: "Liverpool Street Station",
+                value: 200,
+                housePrice: null,
+                color: WHITE,
+                picture: "assets/usa/liverpool_street_station.jpg",
+                rentScheme: [25,50,100,200],
+            },
+            37: {
+                title: "Park City",
+                value: 350,
+                housePrice: 175,
+                color: BLUE,
+                picture: "assets/usa/park_city.jpg",
+                rentScheme: [0,35,175,500,1100,1300,1500],
+            },
+            39: {
+                title: "Boardwalk",
+                value: 400,
+                housePrice: 200,
+                color: BLUE,
+                picture: "assets/usa/boardwalk.jpg",
+                rentScheme: [0,50,200,600,1400,1700,2000],
+            },
+        },
+        currency: "$",
+        multiplier: 1,
     },
-};
-
-let rentScheme = function(args) {
-    
 };
 
 let CARDS = {
@@ -367,7 +618,7 @@ let CARDS = {
                 }
             },
             6: {
-                desc: `Advance to Mayfair`,
+                desc: `Advance to ${gameModes[gameMode]["locations"][39]}`,
                 image: "assets/mascot/",
                 action: function() {
 
