9cf514f96126a0df393d21d77d22a45efa10a06b
Shubham Anand <shubham_chess@live.com>
2019-02-20 21:02:26 +0530

    fix dict key

--- Game/setup.js
+++ Game/setup.js
@@ -5,7 +5,7 @@ function boardSetup(gameMode="UK") {
     // type 1 sqaures
 
     for (let key in boardMap) {
-        let square = new Square(1, key, boardMap[key]["title"], boardMap[key]["picture"], boardMap[key]["color"], boardMap[key]["value"], boardMap[key]["housePrice"], boardMap[key]["rent"])
+        let square = new Square(1, key, boardMap[key]["title"], boardMap[key]["picture"], boardMap[key]["color"], boardMap[key]["value"], boardMap[key]["housePrice"], boardMap[key]["rentScheme"])
         board[key] = square;
     }
 
