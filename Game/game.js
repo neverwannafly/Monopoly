ac3e18a7ac79ab1aeeddf46242d133a9d5744393
Shubham Anand <shubham_chess@live.com>
2019-03-20 08:28:26 +0530

    make number of players dynamic

--- Game/game.js
+++ Game/game.js
@@ -9,7 +9,7 @@ class Game {
         this.dices = dicesSetup();
         // the playing order of players is same as indexing of the players array
         this.players = playerSetup(playersArr);
-        this.playerPositions = [0, 0, 0, 0] // Initially all players start at GO
+        this.playerPositions = new Array(this.players.length).fill(0); // Initially all players start at GO
         // Initially player[0] will make his move
         this.currentPlayer = 0;
         this.moves = 0;
@@ -99,7 +99,14 @@ class Game {
         this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
     }
 
-    
+    renderPlayArea() {
+        this.showPlayerData();
+        this.drawBoard();
+    }
+
+    showPlayerData() {
+
+    }
 
     drawBoard() {
         let board = document.getElementById("board");
