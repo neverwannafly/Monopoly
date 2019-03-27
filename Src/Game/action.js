80dc208a9181ab3a9172854120ac8097be29e525
Shubham Anand <shubham_chess@live.com>
2019-03-27 13:42:07 +0530

    Create action.js

---
+++ Src/Game/action.js
@@ -0,0 +1,28 @@
+let PAY_MONEY = {
+	id: 1,
+	
+};
+let MOVE_SPACES = {
+	id: 2,
+};
+let GO_TO_JAIL = {
+	id: 3,
+};
+let RECIEVE_MONEY = {
+	id: 4,
+};
+let PICK_CHANCE = {
+	id: 5,
+};
+let PICK_COMMUNITY_CHEST = {
+	id: 6,
+};
+let PASS_GO = {
+	id: 7,
+};
+let AUCTION = {
+	id: 8,
+};
+let MORTGAGE = {
+	id:: 9,
+};
