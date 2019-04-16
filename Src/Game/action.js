6bff54f1a66c9fcaeccc88b0ceff49cc71ee3c42
Shubham Anand <shubham_chess@live.com>
2019-04-16 11:46:49 +0530

    add data attribute to actions

--- Src/Game/action.js
+++ Src/Game/action.js
@@ -1,27 +1,36 @@
 let MOVE_SPACES = {
 	id: 1,
+	data: null,
 };
 let PAY_MONEY = {
 	id: 2,
+	data: null,
 };
 let RECIEVE_MONEY = {
 	id: 3,
+	data: null,
 };
 let GO_TO_JAIL = {
 	id: 4,
+	data: null,
 };
 let PASS_GO = {
 	id: 5,
+	data: null,
 };
 let PICK_CHANCE = {
 	id: 6,
+	data: null,
 };
 let PICK_COMMUNITY_CHEST = {
 	id: 7,
+	data: null,
 };
 let MORTGAGE = {
 	id: 8,
+	data: null,
 };
 let AUCTION = {
 	id: 9,
+	data: null,
 };
