6e31bb00a38a8a2d52984910f6310e6a6b62cd15
Shubham Anand <shubham_chess@live.com>
2019-07-15 17:24:45 +0530

    add error handler for insufficient funds

---
+++ Src/Game/handler.js
@@ -0,0 +1,11 @@
+let insufficientFundHandler = function(game, amount, type, onSuccess) {
+    try {
+        game.performBankTransaction(amount, type);
+    } catch(error) {
+        return {
+            type: INSUFFICIENT_FUNDS,
+            message: `Insufficient funds to perform transaction.`
+        }
+    }
+    return onSuccess(game);
+}
