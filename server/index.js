7fc32d1bd8c7f206bfc1e6866b3a8a0388111af8
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-06-14 17:13:19 +0530

    restructure project

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Running on localhost:3000'));
