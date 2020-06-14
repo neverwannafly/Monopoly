8839728b6490727f85460e78d0dadb6978680e99
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-06-14 17:30:41 +0530

    add babel and nodemon to backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, './index.htm'));
 });
 
 app.listen(3000, () => console.log('Running on localhost:3000'));
