9eb2238630fd0635d76d56a13962f595804b320f
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-06-14 21:06:38 +0530

    setup react

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev'
+
 const app = express();
+const port = process.env.PORT || 3000;
+const dist_dirc = path.join(__dirname, '../dist');
+const html_file = path.join(dist_dirc, 'index.html');
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.htm'));
+  res.sendFile(html_file);
 });
 
-app.listen(3000, () => console.log('Running on localhost:3000'));
+app.listen(port, () => console.log('Listening on: http://localhost:3000'));
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+export default () => {
+  return (
+    <h1>Hello from react</h1>
+  )
+};
